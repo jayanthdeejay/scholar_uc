ba90b33adb3b7c1105b1ad3893eb1d4e65229a25
Glen Horton <hortongn@ucmail.uc.edu>
2017-05-01 12:20:38 -0400

    Set URL to scholar-beta.uc.edu

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module ScholarUc
 
     config.exceptions_app = self.routes
 
-    config.application_root_url = 'http://localhost:3000'
+    config.application_root_url = 'http://scholar-beta.uc.edu'
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('services')
     config.autoload_paths << Rails.root.join('jobs')
