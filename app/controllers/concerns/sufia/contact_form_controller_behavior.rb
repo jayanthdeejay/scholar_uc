8f46793296e016f5b2ce542b13969b0fa120130f
Glen Horton <hortongn@ucmail.uc.edu>
2017-05-01 12:19:41 -0400

    Disable captcha on contact form

--- app/controllers/concerns/sufia/contact_form_controller_behavior.rb
+++ app/controllers/concerns/sufia/contact_form_controller_behavior.rb
@@ -12,13 +12,12 @@ module Sufia
 
     def create
       # not spam and a valid form
-      if @contact_form.valid? && passes_captcha_or_is_logged_in?
+      if @contact_form.valid?
         ContactMailer.contact(@contact_form).deliver_now
         flash.now[:notice] = 'Thank you for your message!'
         @contact_form = ContactForm.new
       else
         flash.now[:error] = 'Sorry, this message was not sent successfully. '
-        flash.now[:error] << FAIL_NOTICE unless passes_captcha_or_is_logged_in?
         flash.now[:error] << @contact_form.errors.full_messages.map(&:to_s).join(", ")
       end
       render :new
@@ -26,12 +25,6 @@ module Sufia
       logger.error("Contact form failed to send: #{e.inspect}")
       flash.now[:error] = 'Sorry, this message was not delivered.'
       render :new
-    end
-
-    def verify_google_recaptcha(_key, response)
-      status = `curl "https://www.google.com/recaptcha/api/siteverify?secret=#{CAPTCHA_SERVER['secret_key']}&response=#{response}"`
-      hash = JSON.parse(status)
-      hash["success"] == true ? true : false
     end
 
     protected
@@ -45,9 +38,5 @@ module Sufia
         params.require(:sufia_contact_form).permit(:contact_method, :category, :name, :email, :subject, :message)
       end
 
-      def passes_captcha_or_is_logged_in?
-        return true if current_user.present?
-        verify_google_recaptcha(CAPTCHA_SERVER['secret_key'], params["g-recaptcha-response"])
-      end
   end
 end
--- config/initializers/load_captcha_configs.rb
+++
@@ -1,2 +0,0 @@
-# frozen_string_literal: true
-CAPTCHA_SERVER = YAML.load_file(Rails.root.join('config', 'recaptcha.yml'))[Rails.env]
